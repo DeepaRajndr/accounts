2dc4c36fe9fef04f8b04a85318f6ae4dc82ce447
DeepaRajndr <133270634+DeepaRajndr@users.noreply.github.com>
2023-07-07 13:19:24 -0400

    Added Micrometer and Zipkin

--- src/main/java/com/dpr/accounts/controller/AccountsController.java
+++ src/main/java/com/dpr/accounts/controller/AccountsController.java
@@ -2,6 +2,8 @@ package com.dpr.accounts.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,13 +19,13 @@ import com.dpr.accounts.service.client.LoansFeignClient;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
-import com.netflix.discovery.converters.Auto;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 public class AccountsController {
+
+    private static final Logger logger = LoggerFactory.getLogger(AccountsController.class);
 
     @Autowired
     private AccountsRepository accountsRepository;
@@ -62,6 +64,7 @@ public class AccountsController {
     @CircuitBreaker(name = "detailsForCustomerSupportApp", fallbackMethod = "myCustomerDetailsFallback")
     public CustomerDetails myCustomerDetails(@RequestHeader("dpr-correlation-id") String correlationid,
             @RequestBody Customer customer) {
+        logger.info("myCustomerDetails() method started");
         Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
         List<Loans> loans = loansFeignClient.getLoansDetails(correlationid, customer);
         List<Cards> cards = cardsFeignClient.getCardDetails(correlationid, customer);
@@ -70,7 +73,7 @@ public class AccountsController {
         customerDetails.setAccounts(accounts);
         customerDetails.setLoans(loans);
         customerDetails.setCards(cards);
-
+        logger.info("myCustomerDetails() method ended");
         return customerDetails;
 
     }
