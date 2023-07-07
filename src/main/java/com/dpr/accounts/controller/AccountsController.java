37114cf654a957802abb184345dc0fe8e4f418ad
DeepaRajndr <133270634+DeepaRajndr@users.noreply.github.com>
2023-07-06 21:18:05 -0400

    Addted the TraceID update in Headers logic

--- src/main/java/com/dpr/accounts/controller/AccountsController.java
+++ src/main/java/com/dpr/accounts/controller/AccountsController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dpr.accounts.config.AccountsServiceConfig;
@@ -59,10 +60,11 @@ public class AccountsController {
 
     @PostMapping("/myCustomerDetails")
     @CircuitBreaker(name = "detailsForCustomerSupportApp", fallbackMethod = "myCustomerDetailsFallback")
-    public CustomerDetails myCustomerDetails(@RequestBody Customer customer) {
+    public CustomerDetails myCustomerDetails(@RequestHeader("dpr-correlation-id") String correlationid,
+            @RequestBody Customer customer) {
         Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
-        List<Loans> loans = loansFeignClient.getLoansDetails(customer);
-        List<Cards> cards = cardsFeignClient.getCardDetails(customer);
+        List<Loans> loans = loansFeignClient.getLoansDetails(correlationid, customer);
+        List<Cards> cards = cardsFeignClient.getCardDetails(correlationid, customer);
 
         CustomerDetails customerDetails = new CustomerDetails();
         customerDetails.setAccounts(accounts);
@@ -73,9 +75,10 @@ public class AccountsController {
 
     }
 
-    public CustomerDetails myCustomerDetailsFallback(@RequestBody Customer customer, Throwable t) {
+    public CustomerDetails myCustomerDetails(@RequestHeader("dpr-correlation-id") String correlationid,
+            @RequestBody Customer customer, Throwable t) {
         Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
-        List<Loans> loans = loansFeignClient.getLoansDetails(customer);
+        List<Loans> loans = loansFeignClient.getLoansDetails(correlationid, customer);
 
         CustomerDetails customerDetails = new CustomerDetails();
         customerDetails.setAccounts(accounts);
--- src/main/java/com/dpr/accounts/service/client/CardsFeignClient.java
+++ src/main/java/com/dpr/accounts/service/client/CardsFeignClient.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,5 +15,6 @@ import com.dpr.accounts.model.Cards;
 public interface CardsFeignClient {
 
     @RequestMapping(method = RequestMethod.POST, value = "myCards", consumes = "application/json")
-    List<Cards> getCardDetails(@RequestBody Customer customer);
+    List<Cards> getCardDetails(@RequestHeader("dpr-correlation-id") String correlationid,
+            @RequestBody Customer customer);
 }
